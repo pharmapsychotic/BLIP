236d8bd3c2ff266d4b23fce1ce74ffd3f6618e6e
Justin DuJardin <justin@dujardinconsulting.com>
2022-12-23 17:41:29 -0800

    fix: output a sane pypi package that goes into "blip" folder
     - I think the main problem stemmed from the files not being in a subfolder with the module name.

--- setup.py
+++ setup.py
@@ -1,22 +1,49 @@
-import os
-
-import pkg_resources
+from pathlib import Path
 from setuptools import setup, find_packages
 
-setup(
-    name="blip",
-    py_modules=["blip"],
-    version="1.0",
-    description="",
-    author="salesforce",
-    packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
-    include_package_data=True,
-    package_data={"blip": ["configs/*.json", "*.json"]},
-    extras_require={'dev': ['pytest']},
-)
+
+def setup_package():
+
+    package_name = "blip"
+    root = Path(__file__).parent.resolve()
+    print("roo", root)
+
+    with open(root / "README.md", "r") as fh:
+        long_description = fh.read()
+
+    with open(root / "requirements.txt") as file:
+        REQUIRED_MODULES = [line.strip() for line in file]
+
+    with open(root / "requirements-dev.txt") as file:
+        DEVELOPMENT_MODULES = [line.strip()
+                               for line in file if "-e" not in line]
+
+    extras = {"dev": DEVELOPMENT_MODULES}
+    extras["all"] = [item for group in extras.values() for item in group]
+
+    setup(
+        name=package_name,
+        description="",
+        author="salesforce",
+        author_email="",
+        url="",
+        version="",
+        license="",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        install_requires=REQUIRED_MODULES,
+        packages=find_packages(),
+        extras_require=extras,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Development Status :: 2 - Pre-Alpha",
+        ],
+        python_requires=">=3.6",
+        include_package_data=True,
+    )
+
+
+if __name__ == "__main__":
+    setup_package()
